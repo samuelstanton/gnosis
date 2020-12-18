2325ff91bc8fa8acac570db368644611e1e26012
samuelstanton <ss3765@cornell.edu>
2020-12-17 18:00:39 -0600

    refactor DCGAN, add SNGAN

--- gnosis/models/dcgan.py
+++ gnosis/models/dcgan.py
@@ -2,14 +2,44 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from upcycle.cuda import try_cuda
+import hydra
+
+
+class DCGAN(nn.Module):
+    def __init__(self, gen_cfg, disc_cfg, **kwargs):
+        super().__init__()
+        self.generator = hydra.utils.instantiate(gen_cfg)
+        self.discriminator = hydra.utils.instantiate(disc_cfg)
+
+    def gen_backward(self, batch_size):
+        fake_samples = self.generator.sample(batch_size)
+        fake_probs = self.discriminator(fake_samples)
+        labels = try_cuda(torch.full((batch_size,), 1.))
+        gen_loss = F.binary_cross_entropy(fake_probs, labels)
+        gen_loss.backward()
+        return gen_loss, fake_samples
+
+    def disc_backward(self, real_samples, fake_samples):
+        batch_size = real_samples.size(0)
+        real_probs = self.discriminator(real_samples)
+        labels = try_cuda(torch.full((batch_size,), 1.))
+        real_loss = F.binary_cross_entropy(real_probs, labels)
+        real_loss.backward()
+
+        fake_probs = self.discriminator(fake_samples)
+        labels = try_cuda(torch.full((batch_size,), 0.))
+        fake_loss = F.binary_cross_entropy(fake_probs, labels)
+        fake_loss.backward()
+        return real_loss + fake_loss
 
 
 class DCGenerator(nn.Sequential):
     def __init__(self, nc=3, nz=100, ngf=64, output_dim=64, **kwargs):
         super(DCGenerator, self).__init__()
         self.ngpu = torch.cuda.device_count()
-        self.input_dim = nz
+        self.z_dim = nz
         self.output_dim = output_dim
         modules = [
             # input is Z, going into a convolution
@@ -56,9 +86,19 @@ class DCGenerator(nn.Sequential):
         return super().forward(input)  # real examples must lie in [-1, 1]
 
     def sample(self, num_samples):
-        z_vecs = torch.randn(num_samples, self.input_dim, 1, 1)
+        z_vecs = self.sample_z(num_samples)
         z_vecs = try_cuda(z_vecs)
         return self(z_vecs)
+
+    def sample_z(self, num_samples):
+        return torch.randn(num_samples, self.z_dim, 1, 1)
+
+    @property
+    def device(self):
+        try: return self._device
+        except AttributeError:
+            self._device = next(self.parameters()).device
+            return self._device
 
 
 class DCDiscriminator(nn.Sequential):
@@ -98,7 +138,7 @@ class DCDiscriminator(nn.Sequential):
         self.apply(weights_init)
 
     def forward(self, inputs):
-        return super().forward(inputs)
+        return super().forward(inputs).view(-1)
 
 
 def weights_init(m):
