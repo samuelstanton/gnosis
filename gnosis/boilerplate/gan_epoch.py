fa6ca8e0df39e2a35243e576bfade72791d8540b
samuelstanton <ss3765@cornell.edu>
2020-12-21 20:51:46 -0600

    increase num_disc_updates, add horizontal flip

--- gnosis/boilerplate/gan_epoch.py
+++ gnosis/boilerplate/gan_epoch.py
@@ -11,6 +11,8 @@ def gan_train_epoch(gan_module, gen_opt, disc_opt, train_loader, test_loader, tr
     tot_gen_loss = tot_disc_loss = 0
     desc = f'[GAN] D-LOSS: {tot_disc_loss:0.4f}, G-LOSS: {tot_gen_loss: 0.4f}'
     prog_bar = tqdm(enumerate(train_loader), total=len(train_loader), desc=desc, leave=True)
+
+    gan_module.train()
     for i, (real_samples, _) in prog_bar:
         real_samples = cuda.try_cuda(real_samples)
         batch_size = real_samples.size(0)
@@ -31,6 +33,7 @@ def gan_train_epoch(gan_module, gen_opt, disc_opt, train_loader, test_loader, tr
         desc = f'[GAN] D-LOSS: {tot_disc_loss / (i + 1):0.4f}, '\
                f'G-LOSS: {tot_gen_loss / (i + 1): 0.4f}'
         prog_bar.set_description(desc, refresh=True)
+    gan_module.eval()
 
     metrics = dict(gen_loss=tot_gen_loss / num_batches, disc_loss=tot_disc_loss / num_batches)
     # get FID and IS
