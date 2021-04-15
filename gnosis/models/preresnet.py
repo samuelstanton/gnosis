800c450cf84db7160971eacbedbe7bdb67755262
Pavel Izmailov <izmailovpavel@gmail.com>
2021-04-15 14:45:49 -0400

    adding interpolate_net back in

--- gnosis/models/preresnet.py
+++ gnosis/models/preresnet.py
@@ -3,9 +3,43 @@
     ported from https://github.com/bearpaw/pytorch-classification/blob/master/models/cifar/preresnet.py
 """
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+
+from collections import OrderedDict
+from upcycle import cuda
+
+
+def interpolate_net(net, ckpt_state_dict, distance_ratio, dataloader, freeze_bn):
+    interp_state_dict = OrderedDict()
+    for (net_key, net_param), (ckpt_key, ckpt_param) in zip(net.state_dict().items(), ckpt_state_dict.items()):
+        interp_state_dict[net_key] = (
+            distance_ratio * net_param + (1 - distance_ratio) * ckpt_param
+        )
+    net.load_state_dict(interp_state_dict)
+    if freeze_bn:
+        return net
+
+    # update batchnorm running stats
+    net.train()
+    for inputs, _ in dataloader:
+        with torch.no_grad():
+            net(cuda.try_cuda(inputs))
+    net.eval()
+    return net
+
+
+def freeze_batchnorm(net):
+    for m in net.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.training = False
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
 
 
 def conv3x3(in_planes, out_planes, stride=1):
