3c252288a1e2aabd5039716929901153184a6f1c
samuelstanton <ss3765@cornell.edu>
2021-05-22 09:32:45 -0400

    add synth aug plotting nb

--- gnosis/boilerplate/distillation_epoch.py
+++ gnosis/boilerplate/distillation_epoch.py
@@ -94,7 +94,6 @@ def distillation_epoch(student, train_loader, optimizer, lr_scheduler, epoch, mi
 
     lr_scheduler.step()
     if hasattr(loss_fn.base_loss, 'step'):
-        print(f'beta: {loss_fn.base_loss.beta}')
         loss_fn.base_loss.step()
     ece = expected_calibration_err(*ece_stats, num_samples=total)
     metrics = dict(
